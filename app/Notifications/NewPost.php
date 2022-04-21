93465cc1dfbee248b2428808c0e22a40e9807089
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-04-21 16:35:35 +0800

    Notification formatting

---
+++ app/Notifications/NewPost.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewPost extends Notification
+{
+    use Queueable;
+
+    /**
+     * @var Post
+     */
+    protected $post;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Post $post
+     * @return void
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('New Post from ' . $this->post->website->name)
+                    ->line('A new post has been published on ' . $this->post->website->name . '.')
+                    ->line('Title: ' . $this->post->title)
+                    ->line($this->post->content . '...')
+                    ->action('Read post!', $this->post->url)
+                    ->line('Copyright © ' . date('Y') . ' ' . $this->post->website->name . '.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
