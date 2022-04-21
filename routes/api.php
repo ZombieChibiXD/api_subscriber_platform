c6ac0878395aefec52148f219ccef192f78e5468
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-04-21 15:54:50 +0800

    Added All routes for API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WebsiteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('users', UserController::class);
 
 
-Route::resource('websites', 'Api\WebsiteController');
+// ----  IN PROGRESS ---
+
+Route::resource('websites', WebsiteController::class);
+
+Route::group(['prefix' => 'websites/{website}'], function () {
+    Route::post('subscribe', SubscriptionController::class.'@store');
+    Route::get('subscribed', SubscriptionController::class.'@show');
+    Route::delete('unsubscribe', SubscriptionController::class.'@destroy');
+    Route::get('subscribers', SubscriptionController::class.'@index');
+
+    Route::resource('/posts', PostController::class);
+});
+
+
+
 
