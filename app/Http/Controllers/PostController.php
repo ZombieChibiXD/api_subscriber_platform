248bb4d60afe81034d4b067b5fe3a629eedd04e1
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-04-21 15:53:03 +0800

    Fix Posts CRUD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,13 +34,17 @@ class PostController extends Controller
         // Validate the request
         $form = $request->validate([
             'title' => 'required|string|max:255',
-            'body' => 'required|string|max:255',
+            'tags' => 'required|string|max:255',
+            'url' => 'required|url',
+            'content' => 'required|string|max:150',
         ]);
 
         // Create a new post
         $post = new Post;
         $post->title = $form['title'];
-        $post->body = $form['body'];
+        $post->tags = $form['tags'];
+        $post->url = $form['url'];
+        $post->content = $form['content'];
 
         // Assign the post to the website
         $post->website()->associate($website);
@@ -51,15 +55,59 @@ class PostController extends Controller
         return response()->json($post);
     }
 
+
+    /**
+     * Update post.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Models\Website $website
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Website $website, Post $post)
+    {
+        // Validate the request
+        $form = $request->validate([
+            'title' => 'string|max:255',
+            'tags' => 'string|max:255',
+            'url' => 'url',
+            'content' => 'string|max:150',
+        ]);
+
+        // Update the post
+        $post->title = $form['title'] ?? $post->title;
+        $post->tags = $form['tags'] ?? $post->tags;
+        $post->url = $form['url'] ?? $post->url;
+        $post->content = $form['content'] ?? $post->content;
+
+        // Save the post
+        $post->save();
+
+        return response()->json($post);
+    }
+
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \Models\Website $website
+     * @param  \Models\Post $post
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Website $website, Post $post)
     {
-        $post = Post::findOrFail($id);
+        return response()->json($post);
+    }
+
+
+    /**
+     * Destroy the specified resource.
+     *
+     * @param  \Models\Website $website
+     * @param  \Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Website $website, Post $post)
+    {
+        $post->delete();
 
         return response()->json($post);
     }
