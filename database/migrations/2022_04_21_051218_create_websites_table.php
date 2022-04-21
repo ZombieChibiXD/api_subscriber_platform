abb3d07e7741aafe94459b4ff7356c15f9082536
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-04-21 13:27:51 +0800

    API Migrations

---
+++ database/migrations/2022_04_21_051218_create_websites_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            // There can only be 1 website, any duplicates is a problem
+            $table->string('url')->unique();
+            $table->string('description');
+            $table->string('category');
+            // This website belongs to 1 user
+            $table->foreignId('user_id')->constrained('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('websites');
+    }
+}
--- database/migrations/2022_04_21_051621_create_posts_table.php
+++ database/migrations/2022_04_21_051621_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->foreignId('website_id')->constrained('websites');
+            $table->string('title');
+            $table->string('tags');
+            $table->string('url');
+            // Limit post sneak peek to 150 characters
+            $table->string('content', 150);
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
