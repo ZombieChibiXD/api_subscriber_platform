3936c6bc9ddcf8f02c2f3e46ba8a15fbda5e80db
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-04-21 15:53:30 +0800

    Fixed User CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,39 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * Index route. Used to get user by email.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // Validate the request
+        $form = $request->validate([
+            'email' => 'required|string|max:255',
+        ]);
+
+        // Find the user using the email
+        $user = User::where('email', $form['email'])->first();
+
+        if ($user) {
+            return response()->json($user);
+        }
+
+        return response()->json(['message' => 'User not found'], 404);
+    }
+
+    /**
+     * Show User
+     * @param  \Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -17,15 +50,15 @@ class UserController extends Controller
     public function store(Request $request)
     {
         // Validate the request
-        $form = $request->validate([
+        $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|max:255'
         ]);
 
         // Create a new user
         $user = new User;
-        $user->name = $form['name'];
-        $user->email = $form['email'];
+        $user->name = $request->name;
+        $user->email = $request->email;
 
         // Save the user
         $user->save();
@@ -37,19 +70,16 @@ class UserController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Models\User $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
         // Validate the request
         $form = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|max:255'
         ]);
-
-        // Find the user
-        $user = User::findOrFail($id);
 
         // Update the user
         $user->name = $form['name'];
@@ -62,6 +92,35 @@ class UserController extends Controller
     }
 
 
+    /**
+     * Delete User
+     * @param  \Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted', 'user' => $user]);
+    }
+
+    /**
+     * Create route. Not allowed for public
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return response()->json(['message' => 'Not allowed'], 403);
+    }
+
+    /**
+     * Edit route. Not allowed for public
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        return response()->json(['message' => 'Not allowed'], 403);
+    }
 
     public static function findUserByEmailOrCreate(string $email) : User
     {
